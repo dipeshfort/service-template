cd61136ff9676e55adeb1d10e242c746eb5f0763
Yadav Dipesh <dipesh.yadav@tieto.com>
2018-07-15 00:20:12 +0300

    Added list component

---
+++ webfront/src/components/ReminderList.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export class ReminderList extends Component {
+    render() {
+        return (
+            <section>
+                <h1>Reminders</h1>
+                <ul>
+                    <li>Wakeup</li>
+                </ul>
+            </section>
+        );
+    }
+}
--- webfront/src/index.js
+++ webfront/src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import { render } from 'react-dom';
 import './styles.css';
+import { ReminderList } from "./components/ReminderList";
 
 const App = () => {
-    return (<h1> Hello react!</h1>);
+    return (
+        <section>
+            <h1> Hello react!</h1>
+            <ReminderList />
+        </section>
+    );
 };
 
 render(<App />, document.querySelector('#root'));
