2f22cb597ff3caf241185250c823bcd8e6dfdac0
Dipesh Yadav <dipesh.yadav@tieto.com>
2018-07-21 12:34:34 +0300

    Renamed auth

--- auth/src/app.ts
+++
@@ -1,9 +0,0 @@
-import * as express from "express";
-
-export const app: express.Express = express();
-
-app.use('/health', (req: express.Request, resp: express.Response) => {
-    resp.json({
-        message: 'OK'
-    });
-});
