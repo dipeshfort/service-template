0ea8f52ad5f5650b1c6b1cda2d55fa77d8213a2a
Yadav Dipesh <dipesh.yadav@tieto.com>
2018-07-28 19:13:14 +0300

    Version 0.0.1

--- hh-helloworld/routes/index.js
+++ hh-helloworld/routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Hello World!' });
+  res.render('index', { 
+    title: 'Hello HH!', 
+    version: "v1.0.1" 
+  });
 });
 
 module.exports = router;
