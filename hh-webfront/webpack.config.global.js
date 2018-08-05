0209295780fbbfb2e441d3f05148f4bf15665d63
Yadav Dipesh <dipesh.yadav@tieto.com>
2018-08-05 18:07:33 +0300

    Task: Configure production release

---
+++ hh-webfront/webpack.config.global.js
@@ -0,0 +1,22 @@
+module.exports = {
+    mode: "development",
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            }
+        ]
+    },
+    resolve: {
+        extensions: [".js", ".jsx"]
+    }
+}
